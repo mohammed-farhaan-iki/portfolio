6c1e9213aae223d1d9fb32cf2d97ece190ea50ad
mfarhaan2004 <mfarhaan2004@gmail.com>
2025-08-26 16:54:31 +0400

    update portfolio

--- main_files/classic-version/js/main.js
+++ main_files/classic-version/js/main.js
@@ -391,10 +391,27 @@ function workslider(){
       spaceBetween: 30,
       autoplay:{
           delay: 3000,
+          disableOnInteraction: false,
       },
       pagination: {
           el: ".swiper-pagination",
           clickable: true,
+      },
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 1200px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
       },
       });
 }
